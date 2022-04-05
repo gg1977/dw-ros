313101f4bf865496cacf8cc4ac11a934b7b0d012
Gabriel Kiss <gabriel.kiss@gmail.com>
2022-04-05 11:38:57 +0200

    destroy the resized image when needed and formated the camera files

--- src/nv_sensors/include/camera.h
+++ src/nv_sensors/include/camera.h
@@ -45,86 +45,89 @@ namespace nv
    * perform initialization, start and stop of data acquisition
    * from camera sensor.
    */
-  class SensorCamera {
+  class SensorCamera
+  {
 
-    public:
-      /**
-       * @brief Initailization of SensorCamera class
-       * @details This API is required to initailize the SensorCamera class
-       * and provide the Driveworks handle and SAL handle.
-       *
-       * @param context Driveworks SDK handle
-       * @param hal Driveworks SAL handle
-       */
-      void initialize(dwContextHandle_t context, dwSALHandle_t hal);
+  public:
+    /**
+     * @brief Initailization of SensorCamera class
+     * @details This API is required to initailize the SensorCamera class
+     * and provide the Driveworks handle and SAL handle.
+     *
+     * @param context Driveworks SDK handle
+     * @param hal Driveworks SAL handle
+     */
+    void initialize(dwContextHandle_t context, dwSALHandle_t hal);
 
-      /**
-       * @brief Initailization of camera sensor
-       * @details This API is required to initailize the camera sensor
-       * and start data acquitsion
-       *
-       * @param params Driveworks Sensors params list
-       *
-       * @return true if execution successful
-       *         false otherwise
-       */
-      bool start(dwSensorParams params);
+    /**
+     * @brief Initailization of camera sensor
+     * @details This API is required to initailize the camera sensor
+     * and start data acquitsion
+     *
+     * @param params Driveworks Sensors params list
+     *
+     * @return true if execution successful
+     *         false otherwise
+     */
+    bool start(dwSensorParams params);
 
-      /**
-       * @brief Release of camera sensor
-       * @details This API is required to release the camera sensor
-       * and stop data acquitsion
-       *
-       * @return true if execution successful
-       *         false otherwise
-       */
-      bool stop();
+    /**
+     * @brief Release of camera sensor
+     * @details This API is required to release the camera sensor
+     * and stop data acquitsion
+     *
+     * @return true if execution successful
+     *         false otherwise
+     */
+    bool stop();
 
-      /**
-       * @brief Setting of Publisher
-       * @details This API is required to set ros::Publisher for the camera sensor
-       *
-       * @params pub Pointer to ros::Publisher
-       *
-       * @return true if execution successful
-       *         false otherwise
-       */
-      void setPublisher(ros::Publisher* pub) {
-        m_cameraPub = *pub;
-      }
+    /**
+     * @brief Setting of Publisher
+     * @details This API is required to set ros::Publisher for the camera sensor
+     *
+     * @params pub Pointer to ros::Publisher
+     *
+     * @return true if execution successful
+     *         false otherwise
+     */
+    void setPublisher(ros::Publisher *pub)
+    {
+      m_cameraPub = *pub;
+    }
 
-      /**
-       * @brief Query of Sensor state
-       * @details This API is required to quesy the camera sensor state
-       *
-       * @return true if dta acquisition from camera sensor is active
-       *         false otherwise
-       */
-      bool isSensorsRunning() {
-        return m_cameraRun;
-      }
+    /**
+     * @brief Query of Sensor state
+     * @details This API is required to quesy the camera sensor state
+     *
+     * @return true if dta acquisition from camera sensor is active
+     *         false otherwise
+     */
+    bool isSensorsRunning()
+    {
+      return m_cameraRun;
+    }
 
-    private:
-      void run_camera();
+  private:
+    void run_camera();
 
-      dwContextHandle_t m_sdk       = DW_NULL_HANDLE;
-      dwSALHandle_t     m_hal       = DW_NULL_HANDLE;
-      dwSensorHandle_t  m_cameraSensor = DW_NULL_HANDLE;
-      dwSensorHandle_t m_cameraMaster;
-      dwSensorHandle_t m_camera[16];
-      dwImageHandle_t m_rgbaFrame[16] = {DW_NULL_HANDLE};
-      // Frame grab variables
-      dwImageStreamerHandle_t m_streamerNvmediaToCpuProcessed[16] = {DW_NULL_HANDLE};
+    dwContextHandle_t m_sdk = DW_NULL_HANDLE;
+    dwSALHandle_t m_hal = DW_NULL_HANDLE;
+    dwSensorHandle_t m_cameraSensor = DW_NULL_HANDLE;
+    dwSensorHandle_t m_cameraMaster;
+    dwSensorHandle_t m_camera[16];
+    dwImageHandle_t m_rgbaFrame[16] = {DW_NULL_HANDLE};
+    // Frame grab variables
+    dwImageStreamerHandle_t m_streamerNvmediaToCpuProcessed[16] = {DW_NULL_HANDLE};
 
-      float m_shrinkFactor = 2.0f;
-      dwImageHandle_t m_imageResized = DW_NULL_HANDLE;
-      dwImageTransformationHandle_t m_imageTransformationEngine = DW_NULL_HANDLE;
+    float m_shrinkFactor = 2.0f;
+    dwImageHandle_t m_imageResized = DW_NULL_HANDLE;
+    dwImageTransformationHandle_t m_imageTransformationEngine = DW_NULL_HANDLE;
 
-      bool m_cameraRun = false;
+    bool m_cameraRun = false;
 
-      std::thread m_cameraThread;
+    std::thread m_cameraThread;
 
-      ros::Publisher m_cameraPub;
+    ros::Publisher m_cameraPub;
   };
 
 } // namespace nv
--- src/nv_sensors/src/camera.cpp
+++ src/nv_sensors/src/camera.cpp
@@ -9,7 +9,6 @@
  * SPDX-License-Identifier: MIT
  */
 
-
 #include "camera.h"
 #include "sensor_msgs/Image.h"
 #include "sensor_msgs/image_encodings.h"
@@ -18,16 +17,17 @@
 using namespace sensor_msgs;
 
 // macro to easily check for dw errors
-#define CHECK_DW_ERROR(x)                                                                                                                                                                                                   \
-    {                                                                                                                                                                                                                       \
-        dwStatus result = x;                                                                                                                                                                                                \
-        if (result != DW_SUCCESS)                                                                                                                                                                                           \
-        {                                                                                                                                                                                                                   \
-            throw std::runtime_error(std::string("DW Error ") + dwGetStatusName(result) + std::string(" executing DW function:\n " #x) + std::string("\n at " __FILE__ ":") + std::to_string(__LINE__)); \
-        }                                                                                                                                                                                                                   \
-    };
+#define CHECK_DW_ERROR(x)                                                                                                                                                                          \
+  {                                                                                                                                                                                                \
+    dwStatus result = x;                                                                                                                                                                           \
+    if (result != DW_SUCCESS)                                                                                                                                                                      \
+    {                                                                                                                                                                                              \
+      throw std::runtime_error(std::string("DW Error ") + dwGetStatusName(result) + std::string(" executing DW function:\n " #x) + std::string("\n at " __FILE__ ":") + std::to_string(__LINE__)); \
+    }                                                                                                                                                                                              \
+  };
 
-namespace nv {
+namespace nv
+{
 
   void SensorCamera::initialize(dwContextHandle_t context, dwSALHandle_t hal)
   {
@@ -45,28 +45,30 @@ namespace nv {
     // -----------------------------------------
     {
       status = dwSAL_createSensor(&m_camera[0], paramsClient, m_hal);
-      if(status != DW_SUCCESS) {
+      if (status != DW_SUCCESS)
+      {
         ROS_ERROR("Cannot create sensor %s with %s. Error: %s", paramsClient.protocol, paramsClient.parameters, dwGetStatusName(status));
 
-        return false; 
+        return false;
       }
     }
 
-
     dwImageProperties imageProperties{};
     status = dwSensorCamera_getImageProperties(&imageProperties, DW_CAMERA_OUTPUT_NATIVE_PROCESSED, m_camera[0]);
-    if(status != DW_SUCCESS) {
+    if (status != DW_SUCCESS)
+    {
       ROS_ERROR("Error");
 
       dwSAL_releaseSensor(m_camera[0]);
 
       return false;
     }
-    
+
     // create an image to hold the conversion from native to rgba, fit for streaming to gl
     imageProperties.format = DW_IMAGE_FORMAT_RGBA_UINT8;
     status = dwImage_create(&m_rgbaFrame[0], imageProperties, m_sdk);
-    if(status != DW_SUCCESS) {
+    if (status != DW_SUCCESS)
+    {
       ROS_ERROR("Error");
 
       dwSAL_releaseSensor(m_camera[0]);
@@ -74,8 +76,9 @@ namespace nv {
       return false;
     }
 
-    //initialize the image transformation and the resized image
-    if (m_shrinkFactor > 1.0f) {
+    // initialize the image transformation and the resized image
+    if (m_shrinkFactor > 1.0f)
+    {
       dwImageTransformationParameters m_params{false};
       m_params.ignoreAspectRatio = false;
 
@@ -91,21 +94,26 @@ namespace nv {
       CHECK_DW_ERROR(dwImage_create(&m_imageResized, imageProperties, m_sdk));
     }
 
-      // setup streamer for frame grabbing
-      status = dwImageStreamer_initialize(&m_streamerNvmediaToCpuProcessed[0], &imageProperties, DW_IMAGE_CPU, m_sdk);
-      if (status != DW_SUCCESS)
+    // setup streamer for frame grabbing
+    status = dwImageStreamer_initialize(&m_streamerNvmediaToCpuProcessed[0], &imageProperties, DW_IMAGE_CPU, m_sdk);
+    if (status != DW_SUCCESS)
+    {
+      ROS_ERROR("Error");
+      if (m_imageResized)
       {
-        ROS_ERROR("Error");
+        dwImage_destroy(m_imageResized);
+      }
 
-        dwImage_destroy(m_rgbaFrame[0]);
-        dwSAL_releaseSensor(m_camera[0]);
+      dwImage_destroy(m_rgbaFrame[0]);
+      dwSAL_releaseSensor(m_camera[0]);
 
-        return false;
+      return false;
     }
 
     // start camera
     status = dwSensor_start(m_camera[0]);
-    if(status != DW_SUCCESS) {
+    if (status != DW_SUCCESS)
+    {
       ROS_ERROR("Cannot start camera. Error: %s", dwGetStatusName(status));
 
       dwSAL_releaseSensor(m_camera[0]);
@@ -113,7 +121,7 @@ namespace nv {
       return false;
     }
 
-    m_cameraThread   = std::thread(&SensorCamera::run_camera, this);
+    m_cameraThread = std::thread(&SensorCamera::run_camera, this);
     m_cameraRun = true;
 
     return true;
@@ -121,25 +129,34 @@ namespace nv {
 
   bool SensorCamera::stop()
   {
-    if(!m_cameraRun) {
+    if (!m_cameraRun)
+    {
       ROS_WARN("CAMERA sensor not running");
       return false;
     }
 
     m_cameraRun = false;
 
-    if(m_cameraThread.joinable())
+    if (m_cameraThread.joinable())
       m_cameraThread.join();
 
-    if (m_streamerNvmediaToCpuProcessed[0]) {
+    if (m_streamerNvmediaToCpuProcessed[0])
+    {
       dwImageStreamer_release(m_streamerNvmediaToCpuProcessed[0]);
     }
 
-    if (m_rgbaFrame[0]) {
+    if (m_rgbaFrame[0])
+    {
       dwImage_destroy(m_rgbaFrame[0]);
-    } 
+    }
 
-    if (m_camera[0]) {
+    if (m_imageResized)
+    {
+      dwImage_destroy(m_imageResized);
+    }
+
+    if (m_camera[0])
+    {
       dwSAL_releaseSensor(m_camera[0]);
     }
 
@@ -148,71 +165,89 @@ namespace nv {
 
   void SensorCamera::run_camera()
   {
-    while(m_cameraRun) {
+    while (m_cameraRun)
+    {
       dwCameraFrameHandle_t frame;
       dwStatus status = dwSensorCamera_readFrameNew(&frame, 33333, m_camera[0]);
-      if (status == DW_END_OF_STREAM) {
+      if (status == DW_END_OF_STREAM)
+      {
         ROS_WARN("camera sensor end of stream reached.");
         break;
-      } else if (status == DW_TIME_OUT) {
+      }
+      else if (status == DW_TIME_OUT)
+      {
         ROS_WARN("camera sensor readFrame timed-out.");
         continue;
-      } else if (status == DW_NOT_READY) {
+      }
+      else if (status == DW_NOT_READY)
+      {
         ROS_WARN("camera sensor not ready.");
         continue;
-      } 
+      }
 
-      if (status != DW_SUCCESS) {
+      if (status != DW_SUCCESS)
+      {
         ROS_ERROR("camera sensor readFrame failed. Error: %s", dwGetStatusName(status));
         break;
-      } else {
+      }
+      else
+      {
         ROS_INFO("camera sensor readFrame success.");
 
         dwImageHandle_t img;
         dwCameraOutputType outputType = DW_CAMERA_OUTPUT_NATIVE_PROCESSED;
         status = dwSensorCamera_getImage(&img, outputType, frame);
-        if(status != DW_SUCCESS) {
+        if (status != DW_SUCCESS)
+        {
           ROS_ERROR("Error");
           break;
         }
 
         // convert native (yuv420 planar nvmedia) to rgba nvmedia
         status = dwImage_copyConvert(m_rgbaFrame[0], img, m_sdk);
-        if(status != DW_SUCCESS) {
+        if (status != DW_SUCCESS)
+        {
           ROS_ERROR("Error");
           break;
         }
 
         dwImageHandle_t cpuFrame;
-        if (m_shrinkFactor > 1.0f) {
-          //resize image and send it to the streamer
-         dwImageTransformation_copyFullImage(m_imageResized, m_rgbaFrame[0], m_imageTransformationEngine);
-         if(status != DW_SUCCESS) {
+        if (m_shrinkFactor > 1.0f)
+        {
+          // resize image and send it to the streamer
+          dwImageTransformation_copyFullImage(m_imageResized, m_rgbaFrame[0], m_imageTransformationEngine);
+          if (status != DW_SUCCESS)
+          {
             ROS_ERROR("Error image transform");
             break;
           }
           // stream that image to the CPU domain
           CHECK_DW_ERROR(dwImageStreamer_producerSend(m_imageResized, m_streamerNvmediaToCpuProcessed[0]));
-          
+
           // receive the streamed image as a handle
           status = dwImageStreamer_consumerReceive(&cpuFrame, 33000, m_streamerNvmediaToCpuProcessed[0]);
-          if(status != DW_SUCCESS) {
+          if (status != DW_SUCCESS)
+          {
             ROS_ERROR("Error");
             break;
           }
-        } else {
-          //just send the m_rgbaFrame to the streamer
-    
+        }
+        else
+        {
+          // just send the m_rgbaFrame to the streamer
+
           // stream that image to the CPU domain
           status = dwImageStreamer_producerSend(m_rgbaFrame[0], m_streamerNvmediaToCpuProcessed[0]);
-          if(status != DW_SUCCESS) {
+          if (status != DW_SUCCESS)
+          {
             ROS_ERROR("Error");
             break;
           }
 
           // receive the streamed image as a handle
           status = dwImageStreamer_consumerReceive(&cpuFrame, 33000, m_streamerNvmediaToCpuProcessed[0]);
-          if(status != DW_SUCCESS) {
+          if (status != DW_SUCCESS)
+          {
             ROS_ERROR("Error");
             break;
           }
@@ -220,14 +255,16 @@ namespace nv {
 
         dwImageProperties prop;
         status = dwImage_getProperties(&prop, cpuFrame);
-        if (status != DW_SUCCESS) {
+        if (status != DW_SUCCESS)
+        {
           ROS_ERROR("dwImage_getProperties() failed. Error: %s", dwGetStatusName(status));
           break;
         }
 
         dwImageCPU *imgCPU;
         status = dwImage_getCPU(&imgCPU, cpuFrame);
-        if (status != DW_SUCCESS) {
+        if (status != DW_SUCCESS)
+        {
           ROS_ERROR("dwImage_getCPU() failed. Error: %s", dwGetStatusName(status));
           break;
         }
@@ -250,7 +287,7 @@ namespace nv {
 
         dwImageStreamer_consumerReturn(&cpuFrame, m_streamerNvmediaToCpuProcessed[0]);
         dwImageStreamer_producerReturn(nullptr, 33000, m_streamerNvmediaToCpuProcessed[0]);
-        
+
         dwSensorCamera_returnFrame(&frame);
 
         m_cameraPub.publish(image);
@@ -258,4 +295,4 @@ namespace nv {
     }
   }
 
-} //namespace nv
+} // namespace nv
